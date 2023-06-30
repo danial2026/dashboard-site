17aac1f9604dd7796883e5382cbd2a6a0c74ba81
danial <danial@danials.space>
2023-07-01 01:50:35 +0330

    implement a basic dashboard

--- src/App.js
+++ src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-function App() {
+const TopHeader = () => {
+
+  const getCurrentTime = () => {
+    const date = new Date();
+    const time = date.toLocaleTimeString('en-US', {
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true,
+    });
+    const formattedDate = date.toLocaleDateString('en-US', {
+      weekday: 'long',
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+    });
+    return `${time} - ${formattedDate}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="header">
+      <div className="header">
+        <h1 className="header-title">Dashboard</h1>
+        <div className="greeting">
+          <p>Hi, Daniel</p>
+          <p>{getCurrentTime()}</p>
+        </div>
+      </div> 
     </div>
   );
-}
+};
+
+const App = () => {
+  const cardsData = [
+    {
+      id: 0,
+      title: 'Personal Website',
+      image: '/website.png',
+      link: 'http://website.danials.space/',
+    },
+    {
+      id: 1,
+      title: 'Terminal',
+      image: '/terminal.png',
+      link: 'http://terminal.danials.space/',
+    },
+    {
+      id: 2,
+      title: 'The Countdown',
+      image: '/countdown.png',
+      link: 'https://countdown.danials.space/',
+    },
+  ];
+
+  return (
+    <div  >
+      <TopHeader />
+    <div className="app">
+      
+      {cardsData.map((card) => (
+        <a key={card.id} href={card.link} className="card">
+          <img src={card.image} alt="Card Image" className="card-image" />
+          <h3 className="card-title">{card.title}</h3>
+        </a>
+      ))}
+    </div>
+    </div>
+  );
+};
 
 export default App;
